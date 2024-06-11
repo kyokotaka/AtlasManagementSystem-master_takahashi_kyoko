25ed2a3de76ef21353500f22476d41423f3ae16e
kyokotaka <kt.anzu24@gmail.com>
2024-06-11 19:29:54 +0900

    確認依頼

--- app/Http/Controllers/Authenticated/BulletinBoard/PostsController.php
+++ app/Http/Controllers/Authenticated/BulletinBoard/PostsController.php
@@ -31,10 +31,10 @@ class PostsController extends Controller
             ->orWhere('post', 'like', '%'.$request->keyword.'%')->get();
         }else if($request->category_word){
             $sub_category = $request->category_word;
-            $posts = Post::whereHas('')
+            $posts = Post::whereHas('subCategories',function ($query) use($sub_category){
             //$posts = Post::with('user', 'postComments')
-            ->where()
-            get();
+            $query->where('sub_category', $sub_category);
+        });
         }else if($request->like_posts){
             $likes = Auth::user()->likePostId()->get('like_post_id');
             $posts = Post::with('user', 'postComments')
